ff940d3689cfbee314d58c2a4f61656466b2a359
Tobias Brunkhorst <tobias.brunkhorst@phonostar.de>
2013-10-15 10:54:16 +0200

    updated softkeyboard.js and return module

--- www/softkeyboard.js
+++ www/softkeyboard.js
@@ -1,30 +1,24 @@
-(function( cordova ) {
+function SoftKeyboard() {}
 
-  function SoftKeyboard() {}
-
-  SoftKeyboard.prototype.show = function(win, fail) {
-    return cordova.exec(
+SoftKeyboard.prototype.show = function(win, fail) {
+  return cordova.exec(
       function (args) { if(win !== undefined) { win(args); } },
       function (args) { if(fail !== undefined) { fail(args); } },
       "SoftKeyboard", "show", []);
-  };
+};
 
-  SoftKeyboard.prototype.hide = function(win, fail) {
-    return cordova.exec(
+SoftKeyboard.prototype.hide = function(win, fail) {
+  return cordova.exec(
       function (args) { if(win !== undefined) { win(args); } },
       function (args) { if(fail !== undefined) { fail(args); } },
       "SoftKeyboard", "hide", []);
-  };
+};
 
-  SoftKeyboard.prototype.isShowing = function(win, fail) {
-    return cordova.exec(
+SoftKeyboard.prototype.isShowing = function(win, fail) {
+  return cordova.exec(
       function (args) { if(win !== undefined) { win(args); } },
       function (args) { if(fail !== undefined) { fail(args); } },
       "SoftKeyboard", "isShowing", []);
-  };
+};
 
-  if (!window.SoftKeyboard) {
-    window.SoftKeyboard = new SoftKeyboard();
-  }
-
-})( window.cordova );
+module.exports = new SoftKeyboard();
